f98c907850a995a2844eb143f566edb785bf7fb5
Vito Di Bari <vitodibari1995@gmail.com>
2018-01-12 18:07:37 +0100

    generalizzazione dei template e messa a punto iniziale

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,77 @@
 @extends('layouts.app')
 
+@section('title') Home @endsection
+
+@section('style')
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Raleway', sans-serif;
+            font-weight: 100;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 84px;
+        }
+
+        .links > a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 12px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
+@endsection
+
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+    <div class="flex-center position-ref full-height">
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+        <div class="content">
+            <div class="title m-b-md">
+                SensorTool
+            </div>
 
-                    You are logged in!
-                </div>
+            <div class="links">
+                <a href="https://laravel.com/docs">Documentazione</a>
+                <a href="https://laracasts.com">Login</a>
+                <a href="https://laravel-news.com">Registrazione</a>
             </div>
         </div>
+
     </div>
-</div>
 @endsection

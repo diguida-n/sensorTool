6786455700cca82c81acac1257014536436f206b
Nicola Di Guida <nico.d.guida@gmail.com>
2018-07-03 19:01:39 +0200

    test_register_customer

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -2,10 +2,13 @@
 
 namespace Tests\Feature;
 
+use App\Models\Enterprise;
 use App\User;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Crypt;
 use Illuminate\Validation\ValidationException;
 use Tests\TestCase;
 
@@ -13,6 +16,7 @@ class UserTest extends TestCase
 {
 	use DatabaseMigrations;
 
+    private $enterprise;
 	 protected function setUp()
     {
     	parent::setUp();
@@ -22,7 +26,12 @@ class UserTest extends TestCase
             'email' => "admin@sensortool.com",
             'password' => bcrypt("admin"),
         ]);
-	    $u->assignRole('Admin');
+
+        $this->enterprise = Enterprise::create([
+            'businessName' => "Enterprise",
+            'address' => '{"name":"Via Giuseppe Re David","administrative":"Puglia","county":"Bari","city":"Bari","suburb":"Municipio 2","country":"Italia","countryCode":"it","type":"address","latlng":{"lat":41.1132,"lng":16.8762},"postcode":"70100","value":"Via Giuseppe Re David, Bari, Puglia, Italia"}',
+            'vatNumber' => '12345678910',
+        ]);
 
     }
 
@@ -40,5 +49,24 @@ class UserTest extends TestCase
              ->type('admin', 'password')
              ->press('Accedi')
              ->seePageIs('/');
+    }
+
+    public function test_register_as_customer()
+    {
+        $cryptedData = [];
+
+        $cryptedData['role'] = 'Customer';
+        $cryptedData['enterprise_id'] = $this->enterprise->id;
+        $cryptedData['expiring_date'] = Carbon::now('Europe/Rome')->addDay()->toDateTimeString();
+        $cryptedData['email'] = 'customer@sensrTool.com';
+        $cryptedData = Crypt::encryptString(json_encode($cryptedData));
+        $this->visit('/admin/register/'.$cryptedData)
+            ->type('customer','name')
+            ->type('customer','password')
+            ->type('customer','password_confirmation')
+            ->press('Registrati')
+            ->seePageIs('/customer/dashboard');
+
+        ;
     }
 }

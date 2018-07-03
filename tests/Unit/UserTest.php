726f48b5e70617549d29355b57af16f612bac29d
Giampiero Cascarano <giampierocascarano@gmail.com>
2018-07-03 18:23:42 +0200

    test su test

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -1,23 +1,44 @@
 <?php
 
-namespace Tests\Unit;
+namespace Tests\Feature;
 
-use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Validation\ValidationException;
+use Tests\TestCase;
 
 class UserTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    /** @test */
-    public function testBasicExample()
+	use DatabaseMigrations;
+
+	 protected function setUp()
+    {
+    	parent::setUp();
+
+    	$u = User::create([
+            'name' => "Admin",
+            'email' => "admin@sensortool.com",
+            'password' => bcrypt("admin"),
+        ]);
+	    $u->assignRole('Admin');
+
+    }
+
+    public function test_access_to_site()
     {
         $this->visit('/')
-             ->see('Benvenuti sulla nostra Piattaforma!')
-             ->dontSee('Accedi');
+            ->see('Benvenuti sulla nostra Piattaforma!')
+            ->dontSee('Accedi');
+    }
+   
+    public function test_login_as_admin()
+    {	
+    	$this->visit('/admin/login')
+             ->type('admin@sensortool.com', 'email')
+             ->type('admin', 'password')
+             ->press('Accedi')
+             ->seePageIs('/');
     }
 }

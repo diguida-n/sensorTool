2bedbf411e7fee9aa1ba4acd73106edc1fbb2705
Vito Di Bari <vitodibari1995@gmail.com>
2018-07-05 18:29:59 +0200

    sensor test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,9 @@
 namespace Tests;
 
 use App\Exceptions\Handler;
+use App\Models\Enterprise;
+use App\User;
 use Illuminate\Contracts\Debug\ExceptionHandler;
-// use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Laravel\BrowserKitTesting\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
@@ -12,10 +13,36 @@ abstract class TestCase extends BaseTestCase
     use CreatesApplication;
     public $baseUrl = 'http://localhost';
 
+    protected $admin;
+    protected $customer;
+    protected $enterprise;
+
     protected function setUp()
     {
     	parent::setUp();
     	$this->disableExceptionHandling();//https://gist.github.com/adamwathan/125847c7e3f16b88fa33a9f8b42333da
+
+
+        $this->admin = User::create([
+            'name' => "Admin",
+            'email' => "admin@sensortool.com",
+            'password' => bcrypt("admin"),
+        ]);
+        $this->admin->assignRole('Admin');
+
+        $this->enterprise = Enterprise::create([
+            'businessName' => "Enterprise",
+            'address' => '{"name":"Via Giuseppe Re David","administrative":"Puglia","county":"Bari","city":"Bari","suburb":"Municipio 2","country":"Italia","countryCode":"it","type":"address","latlng":{"lat":41.1132,"lng":16.8762},"postcode":"70100","value":"Via Giuseppe Re David, Bari, Puglia, Italia"}',
+            'vatNumber' => '12345678910',
+        ]);
+
+        $this->customer= User::create([
+            'name' => "Customer",
+            'email' => "customer@enterprise.com",
+            'enterprise_id'=> $this->enterprise->id,
+            'password' => bcrypt("customer")
+        ]);
+        $this->customer->assignRole('Customer');
     }
 
     protected function signIn($user = null)	
--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -16,32 +16,9 @@ class UserTest extends TestCase
 {
 	use DatabaseMigrations;
 
-    private $enterprise;
-	 protected function setUp()
+	protected function setUp()
     {
     	parent::setUp();
-
-    	$u = User::create([
-            'name' => "Admin",
-            'email' => "admin@sensortool.com",
-            'password' => bcrypt("admin"),
-        ]);
-        $u->assignRole('Admin');
-
-        $this->enterprise = Enterprise::create([
-            'businessName' => "Enterprise",
-            'address' => '{"name":"Via Giuseppe Re David","administrative":"Puglia","county":"Bari","city":"Bari","suburb":"Municipio 2","country":"Italia","countryCode":"it","type":"address","latlng":{"lat":41.1132,"lng":16.8762},"postcode":"70100","value":"Via Giuseppe Re David, Bari, Puglia, Italia"}',
-            'vatNumber' => '12345678910',
-        ]);
-
-        $customer= User::create([
-            'name' => "Customer",
-            'email' => "customer@enterprise.com",
-            'enterprise_id'=> $this->enterprise->id,
-            'password' => bcrypt("customer")
-        ]);
-        $customer->assignRole('Customer');
-
     }
 
     public function test_access_to_site()
@@ -77,7 +54,7 @@ class UserTest extends TestCase
             ->seePageIs('/customer/dashboard');
     }
 
-     public function test_login_as_customer()
+    public function test_login_as_customer()
     {   
         $this->visit('/admin/login')
              ->type('customer@enterprise.com', 'email')

3b33ba57067f05cce42e577091247132691bd820
Vito Di Bari <vitodibari1995@gmail.com>
2018-07-12 19:31:14 +0200

    fine enterprise

--- tests/Unit/EnterpriseTest.php
+++ tests/Unit/EnterpriseTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Models\Enterprise;
 use App\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -12,13 +13,7 @@ use Tests\TestCase;
 
 class EnterpriseTest extends TestCase
 {
-
 	use DatabaseMigrations;
-	public function setUp($value='')
-	{
-		parent::setUp();
-	}
-
 
 	public function test_only_admin_access_admin_panel()
 	{
@@ -69,49 +64,31 @@ class EnterpriseTest extends TestCase
             ->seePageIs('/admin/enterprise/create');
 	}
 
-
 	public function test_add_new_enterprise()
 	{
-		$this->actingAs($this->admin);
-		$this->visit('/admin/enterprise/create')
-			 ->type('IoT inc.', 'businessName')
-			 ->type('{"name":"Via Edoardo Orabona","administrative":"Puglia","county":"Bari","city":"Bari","suburb":"Municipio 2","country":"Italia","countryCode":"it","type":"address","latlng":{"lat":41.1077,"lng":16.8798},"postcode":"70100","value":"Via Edoardo Orabona, Bari, Puglia, Italia"}', 'address')
-			 ->type('12345678910', 'vatNumber')
-			 ->press('Salva e torna indietro')
-			 ->seePageIs('/admin/enterprise')
-			 ->assertResponseOk();
+		$fields = [];
+		$fields['businessName'] = 'IoT inc.';
+		$fields['address'] = '{"name":"Via Edoardo Orabona","administrative":"Puglia","county":"Bari","city":"Bari","suburb":"Municipio 2","country":"Italia","countryCode":"it","type":"address","latlng":{"lat":41.1077,"lng":16.8798},"postcode":"70100","value":"Via Edoardo Orabona, Bari, Puglia, Italia"}';
+		$fields['vatNumber'] = '12345674910';
 
+		Enterprise::create($fields);
+
+		$this->assertEquals(1, Enterprise::where($fields)->count());
 	}
 
 	public function test_edit_enterprise()
 	{
-		$this->actingAs($this->admin);
-		$this->visit('/admin/enterprise/'.$this->enterprise->id.'/edit')
-		->see($this->enterprise->businessName)
-		->see($this->enterprise->vatNumber)
-		->press('Salva e torna indietro')
-		->seePageIs('/admin/enterprise')
-		->assertResponseOk();
-
-
+		$this->enterprise->vatNumber = '12345674999';
+		$this->enterprise->save();
+		
+		$this->assertEquals(1, Enterprise::where('vatNumber', $this->enterprise->vatNumber)->count());
 	}
 
-	public function test_validation_of_enterprise_fields()
-	{	
+	public function test_delete_enterprise()
+	{
+		$this->enterprise->delete();
 
-		try {
-			$this->actingAs($this->admin);
-			$this->visit('/admin/enterprise/'.$this->enterprise->id.'/edit')
-			 ->type('', 'businessName')
-				->type('{"name":"Via Edoardo Orabona","administrative":"Puglia","county":"Bari","city":"Bari","suburb":"Municipio 2","country":"Italia","countryCode":"it","type":"address","latlng":{"lat":41.1077,"lng":16.8798},"postcode":"70100","value":"Via Edoardo Orabona, Bari, Puglia, Italia"}', 'address')
-				->type('12345678910', 'vatNumber')
-				->press('Salva e torna indietro')
-			 	->seePageIs('/admin/enterprise/create');
-		} catch (ValidationException $e) {
-			
-		}
-			$this->see($this->enterprise->businessName)
-			->assertResponseOk();
-
+		$this->assertEquals(0, count(Enterprise::find($this->enterprise->id)));
 	}
+
 }
